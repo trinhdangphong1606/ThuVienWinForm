3f6ec85a51bdf6a29c65a7c73035e4cf32b207c1
phongtd <phongtd@devblock.net>
2020-06-14 14:58:19 +0700

    Deleted test

--- DTO/Sach.cs
+++ DTO/Sach.cs
@@ -17,7 +17,6 @@ namespace DTO
         public string NoiDungSach { get; set; }
         public int SoLuong { get; set; }
         public string TrangThai { get; set; }
-        public string Test { get; set; }
 
     }
 }
